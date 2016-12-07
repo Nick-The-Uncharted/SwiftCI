f790070e0943da8e9a35cd201a51c7d93869c344
Nick <605820993@qq.com>
2016-12-07 21:52:36 +0800

    init commit

---
+++ SwiftCI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftCI
+//
+//  Created by administrasion on 07/12/2016.
+//  Copyright © 2016 NJU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
